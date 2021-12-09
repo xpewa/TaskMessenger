21e4f06708f8fba5361dad574119f7e79ae7568e
mansurik1 <66423279+mansurik1@users.noreply.github.com>
2021-12-09 19:42:45 +0300

    Update main_task_server.cpp

--- main_task_server.cpp
+++ main_task_server.cpp
@@ -59,8 +59,8 @@ private:
             result += std::to_string(arr[counter].id) + ":" + arr[counter].head + ":" + UDBM.get_user(arr[counter].assigner_id).login
                     + ":" + UDBM.get_user(arr[counter].executor_id).login + ":";
         }
-        result += std::to_string(arr[counter].id) + ":" + arr[counter].head + ":" + UDBM.get_user(arr[counter].assigner_id).login
-                    + ":" + UDBM.get_user(arr[counter].executor_id).login;
+        result += std::to_string(arr[size - 1].id) + ":" + arr[size - 1].head + ":" + UDBM.get_user(arr[size - 1].assigner_id).login
+                    + ":" + UDBM.get_user(arr[size - 1].executor_id).login;
 
         return result;
     }
