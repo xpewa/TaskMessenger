6e4211a058a5ab29c2d8452d18e08738aed88420
Jojobass <Jojobass@github.com>
2021-12-27 19:54:29 +0300

    added get_users_for_task

--- DB/MessageDBManager.h
+++ DB/MessageDBManager.h
@@ -10,14 +10,17 @@ private:
     mysqlx::Session &session;
     mysqlx::Schema schema;
     mysqlx::Table message;
+    mysqlx::Table task;
 public:
-    MessageDBManager(mysqlx::Session &session_) : session(session_), schema(session.getSchema("test", true)), message(schema.getTable("message", true)) {}
+    MessageDBManager(mysqlx::Session &session_) : session(session_), schema(session.getSchema("test", true)), message(schema.getTable("message", true)), task(schema.getTable("task", true)) {}
 
     bool add_message(const Message &message) override;
 
     std::vector<Message> get_messages_for_task_id(int task_id) override;
 
     void drop() override { session.sql("DROP TABLE message;").execute(); }
+
+    std::vector<int> get_users_for_task(int task_id);
 };
 
 
